14065c70adbb362033e0b0ea8d1b67e523969a96
Marty McEnroe <mcwizard1@gmail.com>
2025-03-08 15:40:07 -0600

    Update 07. Deep state.ts

--- Basic Svelte/Reactivity/07. Deep state.ts
+++ Basic Svelte/Reactivity/07. Deep state.ts
@@ -1,14 +1,14 @@
-//initial
+//solved
 <script>
-	let numbers = [1, 2, 3, 4];
+  let numbers = $state([1, 2, 3, 4]);
 
-	function addNumber() {
-		// TODO implement
-	}
+  function addNumber() {
+    numbers.push(numbers.length + 1);
+  }
 </script>
 
 <p>{numbers.join(' + ')} = ...</p>
 
 <button onclick={addNumber}>
-	Add a number
+  Add a number
 </button>
