cd130b1282830630e2d3bcc72acfe09fa284f381
Marty McEnroe <mcwizard1@gmail.com>
2025-03-08 10:56:11 -0600

    Update 07. Deep State.ts

--- Basic Svelte/Reactivity/07. Deep State.ts
+++ Basic Svelte/Reactivity/07. Deep State.ts
@@ -1,1 +1,14 @@
+//initial
+<script>
+	let numbers = [1, 2, 3, 4];
 
+	function addNumber() {
+		// TODO implement
+	}
+</script>
+
+<p>{numbers.join(' + ')} = ...</p>
+
+<button onclick={addNumber}>
+	Add a number
+</button>
