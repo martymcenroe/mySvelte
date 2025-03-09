18581eeeed0f0e460688bb00446a010de2f21cf1
Marty McEnroe <mcwizard1@gmail.com>
2025-03-08 19:25:02 -0600

    Update 08. Derived state.ts
    tabs vs. space? why aren't programmers mad - as in Ahab mad

--- Basic Svelte/Reactivity/08. Derived state.ts
+++ Basic Svelte/Reactivity/08. Derived state.ts
@@ -1,14 +1,14 @@
 \\initial
 <script>
-	let numbers = $state([1, 2, 3, 4]);
-
-	function addNumber() {
-		numbers.push(numbers.length + 1);
-	}
+  let numbers = $state([1, 2, 3, 4]);
+ 
+  function addNumber() {
+    numbers.push(numbers.length + 1);
+  }
 </script>
 
 <p>{numbers.join(' + ')} = ...</p>
 
 <button onclick={addNumber}>
-	Add a number
+  Add a number
 </button>
