329771e40cce14b898345c931ff6611a9f92c645
Marty McEnroe <mcwizard1@gmail.com>
2025-03-11 03:17:31 -0500

    Update 09. Inspecting state.ts

--- Basic Svelte/Reactivity/09. Inspecting state.ts
+++ Basic Svelte/Reactivity/09. Inspecting state.ts
@@ -1,12 +1,15 @@
 \\initial
 <script>
-	let numbers = $state([1, 2, 3, 4]);
-	let total = $derived(numbers.reduce((t, n) => t + n, 0));
+  let numbers = $state([1, 2, 3, 4]);
+  let total = $derived(numbers.reduce((t, n) => t + n, 0));
 
-	function addNumber() {
-		numbers.push(numbers.length + 1);
-		console.log(numbers);
-	}
+  function addNumber() {
+    numbers.push(numbers.length + 1);
+    console.log($state.snapshot(numbers));
+  }
+
+$inspect(numbers).with(console.trace);
+
 </script>
 
 <p>{numbers.join(' + ')} = {total}</p>
