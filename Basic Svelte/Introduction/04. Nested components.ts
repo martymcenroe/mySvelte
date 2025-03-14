f672f9a62dac00929d5a343abfac3933da9da215
Marty McEnroe <mcwizard1@gmail.com>
2025-03-13 20:18:00 -0500

    Update 04. Nested components.ts

--- Basic Svelte/Introduction/04. Nested components.ts
+++ Basic Svelte/Introduction/04. Nested components.ts
@@ -17,3 +17,4 @@
 //Component names are capitalized, to distinguish them from HTML elements.
 
 //Notice that even though Nested.svelte has a <p> element, the styles from App.svelte don't leak in.
+NOTE to self - have to reorganize this. didn't realize there were two files
