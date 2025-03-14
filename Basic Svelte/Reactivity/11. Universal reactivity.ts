d44ba8caba3a0e3a2ef2579d2499d07c5258b7b5
Marty McEnroe <mcwizard1@gmail.com>
2025-03-13 20:17:18 -0500

    Create 11. Universal reactivity.ts

---
+++ Basic Svelte/Reactivity/11. Universal reactivity.ts
@@ -0,0 +1,10 @@
+\\initial
+<script>
+	import Counter from './Counter.svelte';
+</script>
+
+<Counter />
+<Counter />
+<Counter />
+
+  NOTE TO SELF< turn this into a folder and we have three files underneath
